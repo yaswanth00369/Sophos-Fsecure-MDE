ffa05943fa41758a19b25d38ddf7c37b84bf25f5
Yaswanth <yaswanth00369@gmail.com>
2023-11-10 16:21:43 +0530

    Update Sophos-Fsecure-MDE.sh

--- Sophos-Fsecure-MDE.sh
+++ Sophos-Fsecure-MDE.sh
@@ -16,14 +16,18 @@ echo anvuFqZ9an | sudo -S systemctl -t service  | grep "sophos"
 echo anvuFqZ9an | sudo -S systemctl -t service  | grep "mdatp"
 echo anvuFqZ9an | sudo -S systemctl -t service  | grep "f-secure"
 sleep 15
-echo anvuFqZ9an | sudo -S wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-echo anvuFqZ9an | sudo -S dpkg -i google-chrome-stable_current_amd64.deb
+echo anvuFqZ9an | sudo -S wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+echo anvuFqZ9an | sudo -S sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+echo anvuFqZ9an | sudo -S apt-get update
+echo anvuFqZ9an | sudo -S apt-get install google-chrome-stable
 sleep 15
-echo anvuFqZ9an | sudo -S wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_119.0.2151.44-1_amd64.deb
-echo anvuFqZ9an | sudo -S dpkg -i microsoft-edge-stable_119.0.2151.44-1_amd64.deb
+echo anvuFqZ9an | sudo -S curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+echo anvuFqZ9an | sudo -S install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+echo anvuFqZ9an | sudo -S sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+echo anvuFqZ9an | sudo -S rm microsoft.gpg
+echo anvuFqZ9an | sudo -S apt update
+echo anvuFqZ9an | sudo -S apt install microsoft-edge-stable
 sleep 15
 echo anvuFqZ9an | sudo -S snap install teams-for-linux
-sleep 15
-echo anvuFqZ9an | sudo -S rm -rf google-chrome-stable_current_amd64.deb microsoft-edge-stable_119.0.2151.44-1_amd64.deb
 sleep 15
 echo anvuFqZ9an | sudo -S reboot
